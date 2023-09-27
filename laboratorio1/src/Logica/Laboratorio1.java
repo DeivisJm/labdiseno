2f21360092b6361f5e0c5c10a2e489c5e620ce02
DeivisJm <deivisjm1205@gmail.com>
2023-09-27 17:51:14 -0600

    pruebas

---
+++ laboratorio1/src/Logica/Laboratorio1.java
@@ -0,0 +1,21 @@
+package Logica;
+
+import GUI.*;
+
+/**
+ *
+ * @author deivis
+ */
+public class Laboratorio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz inter = new Interfaz();
+        inter.setVisible(true);
+
+    }
+
+}
